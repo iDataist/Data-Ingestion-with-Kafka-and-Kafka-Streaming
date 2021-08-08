57214f9375bb6f6c817fd60d462bd23067ca3abe
Hui Ren <huiren16@gmail.com>
2021-08-08 11:30:51 -0500

    Update producer.py

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -9,7 +9,7 @@ from confluent_kafka.avro import AvroProducer
 
 logger = logging.getLogger(__name__)
 
-BROKER_URL = "PLAINTEXT://localhost:9092,PLAINTEXT://localhost:9093,PLAINTEXT://localhost:9094"
+BROKER_URL = "PLAINTEXT://localhost:9092"
 SCHEMA_REGISTRY_URL = "http://localhost:8081"
 
 
